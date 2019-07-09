366ed0c1ea82380742dbc5b1417bfc1f421af1e7
Tomas Puricelli <tomasp1189@gmail.com>
2019-07-09 07:05:05 +0200

    Updates

---
+++ precache-manifest.704f1118834d5dd3dc374f8f3fe0a824.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607845e17e9da27ecf90d191b9484f0e",
+    "url": "/chaser-test/index.html"
+  },
+  {
+    "revision": "646c56051c7900227a35",
+    "url": "/chaser-test/static/css/main.16f400df.chunk.css"
+  },
+  {
+    "revision": "6d3b406cf66c8b15a061",
+    "url": "/chaser-test/static/js/2.99a4a834.chunk.js"
+  },
+  {
+    "revision": "646c56051c7900227a35",
+    "url": "/chaser-test/static/js/main.928096bf.chunk.js"
+  },
+  {
+    "revision": "32380ae6403d4214052f",
+    "url": "/chaser-test/static/js/runtime~main.af81ee47.js"
+  }
+]);
